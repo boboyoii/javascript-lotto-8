41be5ca8f4bdc2dfc80d6c1689a63325811e7666
boyeon <boyeon6331@gamil.com>
2025-11-02 11:39:30 +0900

    test: Lotto 클래스 기능 테스트 추가 - 당첨 번호 일치 개수 계산 확인 - 보너스 번호 포함 여부 확인

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,36 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('당첨 번호와 일치하는 숫자의 개수를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 7, 8, 9];
+
+    const matchCount = lotto.getMatchCount(winningNumbers);
+
+    expect(matchCount).toBe(3);
+  });
+
+  test('보너스 번호가 로또 번호에 포함되어 있으면 true를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 5;
+
+    const result = lotto.hasBonus(bonusNumber);
+
+    expect(result).toBe(true);
+  });
+
+  test('보너스 번호가 로또 번호에 포함되어 있지 않으면 false를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 10;
+
+    const result = lotto.hasBonus(bonusNumber);
+
+    expect(result).toBe(false);
+  });
 });
