82763291b5e8109d8345d2ac695bad3b0fa23de4
boyeon <boyeon6331@gamil.com>
2025-11-02 15:00:13 +0900

    feat: 등수별 당첨 내역 출력 기능 추가

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { LOTTO_REWARD } from '../constants/LottoConstants.js';
 
 class OuputView {
   showPurchasedLottos(lottos) {
@@ -8,6 +9,25 @@ class OuputView {
       .join('\n');
 
     Console.print(`${header}${lottoLines}`);
+  }
+
+  showRankCounts(counts) {
+    const lines = [
+      '\n당첨 통계',
+      '---',
+      `3개 일치 (${LOTTO_REWARD.FIFTH.toLocaleString()}원) - ${counts.FIFTH}개`,
+      `4개 일치 (${LOTTO_REWARD.FOURTH.toLocaleString()}원) - ${
+        counts.FOURTH
+      }개`,
+      `5개 일치 (${LOTTO_REWARD.THIRD.toLocaleString()}원) - ${counts.THIRD}개`,
+      `5개 일치, 보너스 볼 일치 (${LOTTO_REWARD.SECOND.toLocaleString()}원) - ${
+        counts.SECOND
+      }개`,
+      `6개 일치 (${LOTTO_REWARD.FIRST.toLocaleString()}원) - ${counts.FIRST}개`,
+      '',
+    ].join('\n');
+
+    Console.print(lines);
   }
 }
 
